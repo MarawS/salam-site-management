1f40fd589574681f8263f3aa676c85cf0f5db65a
Marwah Sulaiman <marwa.829.839@gmail.com>
2025-10-12 23:00:18 +0300

    feat: Complete Swagger UI setup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false, // This disables the warning
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = nextConfig
